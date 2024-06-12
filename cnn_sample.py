463cb4add0f265e7c068274fe8a62ddffd808e68
Abdul Haque <132687150+AbdulHaq03@users.noreply.github.com>
2024-06-12 15:18:57 +0500

    Update cnn_sample.py

--- cnn_sample.py
+++ cnn_sample.py
@@ -83,6 +83,9 @@ def sampling_slide(slide_info):
 
     slide_path = os.path.join(args.slide_dir, slide_rpath)
     image_dir = os.path.join(slide_path, scales[args.level])
+
+    tissue_mask = get_tissue_mask(cv2.imread(
+                os.path.join(slide_path, 'Overview.jpg')))
     
     content_mat = cv2.blur(tissue_mask, ksize=args.filter_size, anchor=(0, 0))
     content_mat = content_mat[::args.srstep, ::args.srstep]
